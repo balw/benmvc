cf1283f9eb6cd1b20edfc31b248378aa2260212f
Ben Wells <ben@balw.co.uk>
2019-08-17 12:21:44 +0100

    Version 1

---
+++ app/config/config.php
@@ -0,0 +1,16 @@
+<?php
+
+    // DB Params
+    define('DB_HOST', 'localhost');
+    define('DB_USER', '__USER');
+    define('DB_PASS', '__PASS');
+    define('DB_NAME', '__DBNAME');
+
+    // App Root
+    define('APPROOT', dirname(dirname(__FILE__)));
+    // URL Root
+    define('URLROOT', '__SITEURL');
+    // Site name
+    define('SITENAME', '__SITENAME');
+
+    // update ht access file in public as well
